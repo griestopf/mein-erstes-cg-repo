dddd4288a45f2127303438a309513b5392b8866f
Christoph Müller <chrism@g-graphix.de>
2026-06-02 16:03:30 +0200

    Initial commit

---
+++ mein-erstes-cg/robot.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@export var upper_arm_speed : float = 180
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Hallo, ich bin ready!")
+
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	print(delta)
+	if Input.is_action_pressed("upper_arm_up"):
+		$Base/UpperArm.rotate_x(deg_to_rad(upper_arm_speed) * delta)
+	if Input.is_action_pressed("upper_arm_down"):
+		$Base/UpperArm.rotate_x(deg_to_rad(-upper_arm_speed) * delta)
