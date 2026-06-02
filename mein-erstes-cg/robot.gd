e620e80a5798571a386186e9b4a544f410d1b40f
Christoph Müller <chrism@g-graphix.de>
2026-06-02 17:13:03 +0200

    UI with buttons

--- mein-erstes-cg/robot.gd
+++ mein-erstes-cg/robot.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @export var upper_arm_speed : float = 720
 
+var upper_arm_vel : float = 0;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Hallo, ich bin ready!")
@@ -10,6 +12,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("upper_arm_up"):
-		$Base/UpperArm.rotate_x(deg_to_rad(upper_arm_speed) * delta)
+		upper_arm_vel = upper_arm_speed	
 	if Input.is_action_pressed("upper_arm_down"):
-		$Base/UpperArm.rotate_x(deg_to_rad(-upper_arm_speed) * delta)
+		upper_arm_vel = -upper_arm_speed
+	$Base/UpperArm.rotate_x(deg_to_rad(upper_arm_vel) * delta)
+	upper_arm_vel = 0
+
+func _on_button_up_down() -> void:
+	upper_arm_vel = upper_arm_speed
+
+
+func _on_button_down_down() -> void:
+	upper_arm_vel = -upper_arm_speed
